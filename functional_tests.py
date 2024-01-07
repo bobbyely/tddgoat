2a132b1b0a8c9522d53e9444ba12daff075bcf00
Rob Ely <rob.ely@delwp.vic.gov.au>
2024-01-08 00:23:09 +1100

    did some things

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,21 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.browser = webdriver.Firefox()
 
-assert "Congratulations!" in browser.title
-print("OK")
+    def tearDown(self) -> None:
+        self.browser.quit()
+
+    def test_can_start_a_todo_list(self):
+        # Open localhost on port 8000
+        self.browser.get("http://localhost:8000")
+
+        self.assertIn("To-Do", self.browser.title)
+
+        self.fail("Finish the test!")
+
+
+if __name__ == "__main__":
+    unittest.main()
